868ed4e3c53d5998e756af7f81295dc5b72157a9
Gaurav Sheth <gauravsheth2001@gmail.com>
2024-10-01 19:59:28 -0400

    changed utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import sys
 from pymongo import MongoClient
 
 def get_DB():
-    with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
+    with open(os.path.join(sys.path[0], "app/config.py"), "r") as f:
         content=f.readlines()
     client = MongoClient("mongodb+srv://anishd1910:VEfj1VPH9foeE3NT@cluster0.oagwk.mongodb.net/")
 
