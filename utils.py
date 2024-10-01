b75302237195ca9eb17f6b088282af8d82518088
Gaurav Sheth <80212233+gsheth21@users.noreply.github.com>
2024-10-01 19:34:08 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 def get_DB():
     with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
         content=f.readlines()
-    client = MongoClient("mongodb+srv://ncsuse123:"+content[0]+"@cluster0.vceghwv.mongodb.net/?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://anishd1910:VEfj1VPH9foeE3NT@cluster0.oagwk.mongodb.net/")
 
     
     return client.SEProj2
